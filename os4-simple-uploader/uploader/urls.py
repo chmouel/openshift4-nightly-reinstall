5255e14bb81dd61b6ef63cae0d9952f5994809d7
Chmouel Boudjnah <chmouel@chmouel.com>
2019-12-09 10:29:54 +0000

    Changes to run under nginx/uwsgi
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- os4-simple-uploader/uploader/urls.py
+++ os4-simple-uploader/uploader/urls.py
@@ -14,10 +14,13 @@
 # under the License.
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'(.*)', views.show, name='show'),
-]
+    url(r'^upload', views.upload, name='upload'),
+] + static(
+    "/", document_root=settings.MEDIA_ROOT)
--- os4-simple-uploader/uploader/views.py
+++ os4-simple-uploader/uploader/views.py
@@ -1,7 +1,7 @@
 import os
 
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
 
@@ -12,36 +12,25 @@ def handle_uploaded_file(path, f):
     if path.startswith("/") or '..' in path:
         return
 
-    save_path = os.path.join(settings.MEDIA_ROOT, 'fileuploads', path)
+    save_path = os.path.join(settings.MEDIA_ROOT, path)
     # Security breach?
     if not os.path.abspath(save_path).startswith(settings.MEDIA_ROOT):
         return
-    if os.path.exists(save_path):
-        os.path.remove(save_path)
-    if not os.path.exists(os.path.dirname(save_path)):
-        os.makedirs(os.path.dirname(save_path))
+    if default_storage.exists(save_path):
+        default_storage.delete(save_path)
     default_storage.save(save_path, f['file'])
     return
 
 
 @csrf_exempt
-def index(request):
+def upload(request):
     if request.method == 'POST':
         handle_uploaded_file(request.POST['path'], request.FILES)
+    return HttpResponse("OK")
 
+
+def index(request):
     return HttpResponse("OS4 Install!!!")
-
-
-def show(request, path):
-    if path.startswith("/") or '..' in path:
-        return HttpResponse("denied")
-    fullpath = os.path.join(settings.MEDIA_ROOT, 'fileuploads', path)
-    if not os.path.abspath(fullpath).startswith(settings.MEDIA_ROOT):
-        return
-    if not os.path.exists(fullpath) and not os.path.isfile(fullpath):
-        return HttpResponseNotFound("Page %s not found" % path)
-
-    return HttpResponse(default_storage.open(fullpath).read())
 
 
 # Create your views here.
