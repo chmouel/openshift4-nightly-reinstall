dfc1f9d6126de08f7526ddab7f85bbb89afdf6dc
Chmouel Boudjnah <chmouel@chmouel.com>
2019-12-11 06:13:45 +0000

    Remove whitenoise and django-debug
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- os4-simple-uploader/osinstall/settings.py
+++ os4-simple-uploader/osinstall/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
       'django.middleware.security.SecurityMiddleware',
-      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'osinstall.urls'
