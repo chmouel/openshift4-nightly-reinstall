7003e982961ceac2299b04662a0f985f3a55c270
Chmouel Boudjnah <chmouel@chmouel.com>
2020-01-15 23:37:08 +0100

    Allow downloading unreleased versions, i.e:  `download.sh -d latest-4.3`"
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- binaries/download.sh
+++ binaries/download.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # Chmouel Boudjnah <chmouel@redhat.com>
 version=latest
+base="ocp"
+[[ $1 == "-d" ]] && { base="ocp-dev-preview"  ; shift ;}
 [[ -n ${1} ]] && version=${1}
-URL=https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${version}
+URL=https://mirror.openshift.com/pub/openshift-v4/clients/${base}/${version}
 set -e
-
+DEST=${DEST:-.}
+set -x
 version=$(curl -s ${URL}/release.txt |sed -n '/Version:/ { s/.*:[ ]*//; p ;}')
 
 [[ -z ${version} ]] && {
@@ -23,10 +26,11 @@ case $(uname -o) in
         echo "Could not detect platform: $(uname -o)"
         exit 1
 esac
-
+echo ${URL}/openshift-client-${platform}-${version}.tar.gz
+exit 
 echo -n "Downloading openshift-clients-${version}: "
-curl -sL ${URL}/openshift-client-${platform}-${version}.tar.gz|tar xz -f- oc kubectl
+curl -sL ${URL}/openshift-client-${platform}-${version}.tar.gz|tar -C ${DEST} xz -f- oc kubectl
 echo "Done."
 echo -n "Downloading openshift-installer-${version}: "
-curl -sL ${URL}/openshift-install-${platform}-${version}.tar.gz|tar xz -f- openshift-install
+curl -sL ${URL}/openshift-install-${platform}-${version}.tar.gz|tar -C ${DEST}  xz -f- openshift-install
 echo "Done."
